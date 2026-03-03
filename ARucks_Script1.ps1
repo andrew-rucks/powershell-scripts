b2505dd2122e33a44cdcdb14eeaab653ab13bdfe
Andrew Rucks <74918588+andrew-rucks@users.noreply.github.com>
2026-03-03 11:10:25 -0500

    Initial upload
    scripts 1 and 2

---
+++ ARucks_Script1.ps1
@@ -0,0 +1,150 @@
+﻿#Requires -RunAsAdministrator
+
+# Andrew Rucks
+# 2/18/26
+# ENDPOINT SECURITY CONFIGURATION REPORT
+
+#region Variables
+# section 1
+$DeviceName=""
+$OSVersion=""
+$Reboot=""
+$SignedOnUser=""
+# section 2
+$AcctRestricted="Yes"
+$PWComplexity="N/A (Home Edition)"
+$PWExpiration="N/A (Home Edition)"
+# section 3
+$AutoUpdates="Yes"
+$LastUpdate=""
+# section 4
+$AVInstalled="No"
+$RealTimeProt="No"
+$LastDefUpdate=""
+# section 5
+$FirewallEnabled="No"
+$InboundRules=""
+$OutboundRules=""
+# section 6
+$DiskEncryption="No"
+$RemMedRes="No"
+# section 7
+$TikTokFound="No"
+$VLCFound="No"
+$GoogleFound="No"
+# section 8
+$EventLogs="No"
+$TamperProt="No"
+# section 9
+$LockTimeout="No"
+$SecureBoot="No"
+#endregion
+
+#region InformationGathering
+echo "Gathering Information..."
+$computerinfo = Get-ComputerInfo
+$hotfix = Get-HotFix
+$av = Get-MpComputerStatus
+$frules = Get-NetFirewallRule
+$screensaver = Get-WMIObject Win32_Desktop | Where-Object name -match $env:USERNAME
+
+# section 1
+$DeviceName = $computerinfo.CsName
+$OSVersion = $computerinfo.OsVersion
+$Reboot = $computerinfo.OsLastBootUpTime
+$SignedOnUser = $computerinfo.CsUserName
+
+# section 2
+if ((Get-LocalUser -Name administrator).Enabled){$AcctRestricted = "No"}
+
+# section 3
+if ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoUpdate -ErrorAction Ignore).NoAutoUpdate -EQ $true){$AutoUpdates = "No"}
+$LastUpdate = (Get-HotFix | Sort-Object -Property InstalledOn -Descending)[0].InstalledOn
+
+# section 4
+if ($av.AntivirusEnabled){$AVInstalled = "Yes"}
+if ($av.RealTimeProtectionEnabled){$RealTimeProt = "Yes"}
+$LastDefUpdate = $av.AntivirusSignatureLastUpdated
+
+# section 5
+if ((Get-NetFirewallProfile)[0].Enabled -AND (Get-Service -Name mpssvc | Where-Object -Property Status -EQ "Running")){$FirewallEnabled = "Yes"} #checks if service is running just in case
+$InboundRules = (Get-NetFirewallRule | Where-Object -Property Direction -EQ "Inbound").Name[0..4] -join ", "
+$OutboundRules = (Get-NetFirewallRule | Where-Object -Property Direction -EQ "Outbound").Name[0..4] -join ", "
+
+# section 6
+if ((Get-BitLockerVolume -MountPoint "C:").ProtectionStatus -EQ "On"){$DiskEncryption = "Yes"} #needs administrator
+if ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices" -Name DenyAll -ErrorAction Ignore).DenyAll -EQ $true){$RemMedRes = "Yes"}
+
+# section 7
+if ((Get-Package | Where-Object -Property Name -EQ "Google Chrome") -NE $null){$GoogleFound = "Yes"}
+if ((Get-Package | Where-Object -Property Name -like "*TikTok*") -NE $null){$TikTokFound = "Yes"}
+if ((Get-Package | Where-Object -Property Name -EQ "VLC media player") -NE $null){$VLCFound = "Yes"}
+
+# section 8
+if ((Get-EventLog -LogName System -Newest 1).TimeGenerated.Date -EQ (Get-Date).Date){$EventLogs = "Yes"} #sees if an event log was generated today
+if ($av.IsTamperProtected){$TamperProt = "Yes"} #I don't know if this affects logging, but its the only tamper protection I could find...
+
+# section 9
+if ($screensaver.ScreenSaverActive -AND $screensaver.ScreenSaverSecure){$LockTimeout = "Yes"}
+if (Confirm-SecureBootUEFI){$SecureBoot = "Yes"} #needs administrator
+
+echo "Generating report..."
+#endregion
+
+#region HTML
+"<!DOCTYPE html>
+<html>
+<head>
+<title>$DeviceName</title>
+</head>
+<body>
+
+<h1>Endpoint Security Configuration Report</h1>
+
+<h2>1. System Information</h2>
+<p><b>Device Name: </b>$DeviceName</p>
+<p><b>OS Version: </b>$OSVersion</p>
+<p><b>Last Reboot Time: </b>$Reboot</p>
+<p><b>Signed-on User: </b>$SignedOnUser</p>
+
+<h2>2. Accounts & Authentication</h2>
+<p><b>Local Admin Accounts Disabled: </b>$AcctRestricted</p>
+<p><b>Password Complexity Enforced: </b>$PWComplexity</p>
+<p><b>Password Expiration Policy: </b>$PWExpiration</p>
+
+<h2>3. Patch & Update Status</h2>
+<p><b>Automatic Updates Enabled: </b>$AutoUpdates</p>
+<p><b>Last OS Update: </b>$LastUpdate</p>
+
+<h2>4. Endpoint Protection</h2>
+<p><b>Antivirus Installed: </b>$AVInstalled</p>
+<p><b>Real-time Protection Enabled: </b>$RealTimeProt</p>
+<p><b>Last AV Signature Update: </b>$LastDefUpdate</p>
+
+<h2>5. Firewall & Network Security</h2>
+<p><b>Firewall Enabled: </b>$FirewallEnabled</p>
+<p><b>Inbound Rules Reviewed: </b>$InboundRules</p>
+<p><b>Outbound Rules Reviewed: </b>$OutboundRules</p>
+
+<h2>6. Disk & Data Protection</h2>
+<p><b>Disk Encryption Enabled: </b>$DiskEncryption</p>
+<p><b>Removable Media Restrictions: </b>$RemMedRes</p>
+
+<h2>7. Application Security</h2>
+<p>Unauthorized Software Check</p>
+<p><b>TikTok: </b>$TikTokFound</p>
+<p><b>VLC: </b>$VLCFound</p>
+<p><b>Google Chrome: </b>$GoogleFound</p>
+
+<h2>8. Logging & Monitoring</h2>
+<p><b>Event Logs Enabled: </b>$EventLogs</p>
+<p><b>Tamper Protection Enabled: </b>$TamperProt</p>
+
+<h2>9. Physical Security</h2>
+<p><b>Device Lock Timeout Configured: </b>$LockTimeout</p>
+<p><b>Secure-boot Enabled: </b>$SecureBoot</p>
+
+</body>
+</html>" > ./ARucks_GeneratedReport.html
+start ./ARucks_GeneratedReport.html
+#endregion
---
+++ ARucks_Script2.ps1
@@ -0,0 +1,77 @@
+﻿# Andrew Rucks
+# 3/3/26
+# For CIT 241  - Systems Programming
+# CALCULATE BROADCAST OR NETWORK ID
+
+function Main ()
+{
+    echo "The following operations are available...
+    `t1. Calculate a broadcast address. You will need the subnet mask and network ID.
+    `t2. Calculate a network ID. You will need a host ID and the subnet mask.`n"
+
+    # determine user's choice for which function to use
+    switch (Read-Host -Prompt "Choose the operation you want by entering a number from above")
+    {
+        "1" {Calculate-Broadcast}
+        "2" {Calculate-Network}
+        default
+        {
+            echo "!!! Invalid option selected.`n"
+            Main #restarts script
+        }
+    }
+}
+
+function Calculate-Broadcast ()
+{
+    echo "Selected broadcast address"
+
+    # ask for network ID and network mask
+    $netid, $netmask = Ask-Addresses "network ID" "network/subnet mask"
+
+    # calculates broadcast address
+    $bc = [ipaddress]"1.1.1.1" #creates basic IP address object
+    $bc.Address = ((-BNOT $netmask.Address) -BOR ($netid.Address)) #performs bitwise operations on the integer forms of addresses to get broadcast addr. then uses IP object to convert to string
+
+    # prints results
+    echo "`nResult (Broadcast Address): $($bc.IPAddressToString)"
+}
+
+function Calculate-Network ()
+{
+    echo "Selected network ID"
+
+    # asks for host address and network mask
+    $hostaddr, $netmask = Ask-Addresses "host address" "network/subnet mask"
+
+    # calculates network ID
+    $nid = [ipaddress]"1.1.1.1" #creates basic IP address object
+    $nid.Address = ($hostaddr.Address -BAND $netmask.Address) #performs bitwise operations on the integer forms of addresses to get network ID. then uses IP object to convert to string
+
+    # prints results
+    echo "`nResult (Network ID): $($nid.IPAddressToString)"
+}
+
+function Ask-Addresses ($prompt1, $prompt2)
+{
+    # repeatedly asks for addresses until they are given in the right format.
+    do
+    {
+        try #to convert to IP address type
+        {
+            [ipaddress]$addr1 = Read-Host -Prompt "Enter the $prompt1 (xxx.xxx.xxx.xxx)"
+            [ipaddress]$addr2 = Read-Host -Prompt "Enter the $prompt2 (xxx.xxx.xxx.xxx)"
+            $ipisvalid = $true
+        }
+        catch
+        {
+            Write-Host "!!! Error: IP address formatted incorrectly. Please try again...`n"
+        }
+    }
+    until ($ipisvalid)
+
+    return @($addr1, $addr2)
+}
+
+# calls first function
+Main
